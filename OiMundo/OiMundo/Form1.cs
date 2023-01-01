ca7c4fe2cfd32ef9e39d9e8283bad43f0493ebc0
Borat2021 <wborgesdeoliveira@hotmail.com>
2023-01-01 15:47:42 -0300

    Capitulo 3 apostilha do Curso C

--- OiMundo/OiMundo/Form1.cs
+++ OiMundo/OiMundo/Form1.cs
@@ -10,13 +10,25 @@ namespace OiMundo
 {
     public partial class Form1 : Form
     {
+        //Declarando a variável
+        string mensagem = "BEM VINDO AO BANCO SENAI!";       
+        int idadeWillyam = 24;
+        int idadeWenio = 19;
+        int idadeKywter = 16;
+        double mediadosfilhos;
+        
+        
         public Form1()
         {
             InitializeComponent();
+            mediadosfilhos = (idadeWillyam + idadeWenio + idadeKywter)/3;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show(mensagem);
+            MessageBox.Show(" Willyam tem " + idadeWillyam+ " anos,\n Wenio tem " + idadeWenio + " anos, \n Kywter tem " +idadeKywter + " anos.");
+            MessageBox.Show("A média dos filhos do Wênder é " + mediadosfilhos + " anos.");
             MessageBox.Show("Olá Mundo");
         }
     }
